e22f0a08e5c58dcfc793cc51f4a410caddf780cb
Alexandre Tolstenko <tolstenko@gmail.com>
2023-08-07 15:27:35 -0400

    change import to require

--- src/whatsapp/whatsapp.controller.ts
+++ src/whatsapp/whatsapp.controller.ts
@@ -9,7 +9,8 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import WhatsApp from 'whatsapp';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const WhatsApp = require('whatsapp');
 
 @Controller('whatsapp')
 export class WhatsappController {
