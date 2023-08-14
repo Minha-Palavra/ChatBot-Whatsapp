76c53a8fe54185aa8e05836bb9a52bafc4501c6b
Alexandre Tolstenko <tolstenko@gmail.com>
2023-08-14 16:41:42 -0400

    fix(history): add message direction

--- src/history/entities/history.entity.ts
+++ src/history/entities/history.entity.ts
@@ -2,9 +2,19 @@ import { AbstractEntity } from '../../shared/abstract.entity';
 import { ApiProperty } from '@nestjs/swagger';
 import { Column, Entity } from 'typeorm';
 import { WebhookObject } from 'whatsapp/build/types/webhooks';
+
+export enum MessageDirection {
+  INCOMING = 'INCOMING',
+  OUTGOING = 'OUTGOING',
+}
+
 @Entity({ name: 'history' })
 export class HistoryEntity extends AbstractEntity {
   @ApiProperty()
   @Column({ type: 'json' })
   message: WebhookObject;
+
+  @ApiProperty({ enum: MessageDirection})
+  @Column({ type: 'enum', enum: MessageDirection })
+  direction: MessageDirection;
 }
